039b8428c293045dd583137efff46361a458bd15
garwmishra <85062023+garwmishra@users.noreply.github.com>
2022-04-25 11:31:12 +0530

    Update bot_alive.py

--- userbot/plugins/assistant/bot_alive.py
+++ userbot/plugins/assistant/bot_alive.py
@@ -3,8 +3,8 @@ from telethon import events
 from userbot import ALIVE_NAME, bot
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Unknown"
-PM_IMG = "https://telegra.ph/file/22e70478bd48568451a1f.jpg"
-pm_caption = "➥ **ASSISTANT IS:** `ONLINE`\n\n"
+PM_IMG = "https://telegra.ph/file/537b5d758e7487497e322.jpg"
+pm_caption = "➥ **ASSISTANT IS:** `ONLIfileNE`\n\n"
 pm_caption += "➥ **SYSTEMS STATS**\n"
 pm_caption += "➥ **Telethon Version:** `1.15.0` \n"
 pm_caption += "➥ **Python:** `3.8.6` \n"
@@ -14,8 +14,8 @@ pm_caption += f"➥ **Version** : `2.0`\n"
 pm_caption += f"➥ **My Boss** : {DEFAULTUSER} \n"
 pm_caption += "➥ **Heroku Database** : `AWS - Working Properly`\n\n"
 pm_caption += "➥ **License** : [GNU General Public License v3.0](github.com/DARK-COBRA/DARKCOBRA/blob/master/LICENSE)\n"
-pm_caption += "➥ **Copyright** : By [DarkCobra](https://t.me/Dark_cobra_support_group)\n"
-pm_caption += "[Assistant By DarkCobra](https://t.me/Dark_cobra_support_group)"
+pm_caption += "➥ **Copyright** : By [KAALBOT](https://t.me/DYNAMO_ARMY)\n"
+pm_caption += "[Assistant By KAALBOT](https://t.me/D3VIL_DYNAMO)"
 
 # only Owner Can Use it
 @tgbot.on(events.NewMessage(pattern="^/alive", func=lambda e: e.sender_id == bot.uid))
